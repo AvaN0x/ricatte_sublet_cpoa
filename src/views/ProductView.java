6793bd6e0bf21420f5a26eb83d30bcffcb826706
AvaN0x <clement.ricatte57@gmail.com>
2020-09-11 15:45:07 +0200

    add update and delete product
    Need to make a method that get automatically a category from the id

--- src/views/ProductView.java
+++ src/views/ProductView.java
@@ -7,42 +7,35 @@ public class ProductView {
     private static ProductController prodController;
 
     public static void openProductMenu() {
-        System.out.println("\n" + 
-            "-- Product menu --" + "\n" + 
-            "0/ Back to main menu" + "\n" +
-            "1/ SELECT" + "\n" + 
-            "2/ INSERT" + "\n" + 
-            "3/ UPDATE" + "\n" + 
-            "4/ DELETE" + "\n"
-        );
+        System.out.println("\n" + "-- Product menu --" + "\n" + "0/ Back to main menu" + "\n" + "1/ SELECT" + "\n"
+                + "2/ INSERT" + "\n" + "3/ UPDATE" + "\n" + "4/ DELETE" + "\n");
         System.out.print("Choice : ");
 
         Scanner scan = new Scanner(System.in);
         String prompted = scan.nextLine();
-	    try {
+        try {
             int submenu = Integer.parseInt(prompted.trim());
             switch (submenu) {
-                case 0 : 
+                case 0:
                     MainView.openMainMenu();
                     break;
-                case 1 : 
+                case 1:
                     printProducts();
                     break;
-                case 2 : 
+                case 2:
                     insertProduct();
                     break;
-                case 3 : 
+                case 3:
                     // updateProduct();
                     break;
-                case 4 : 
+                case 4:
                     // deleteProduct();
                     break;
-                default :
+                default:
                     openProductMenu();
                     break;
             }
-        }
-        catch (NumberFormatException nfe) {	    	
+        } catch (NumberFormatException nfe) {
             System.out.println("NumberFormatException: " + nfe.getMessage());
             openProductMenu();
         }
@@ -52,7 +45,8 @@ public class ProductView {
     public static void printProducts() {
         var Products = prodController.getProducts();
         for (var p : Products) {
-            System.out.println(p.getId() + " : " + p.getNom() + " - " + p.getDescription() + " - " + p.getTarif() + " - " + p.getVisuel() + " - " + p.getCategorie().getId());
+            System.out.println(p.getId() + " : " + p.getNom() + " - " + p.getDescription() + " - " + p.getTarif()
+                    + " - " + p.getVisuel() + " - " + p.getCategorie().getId());
         }
         openProductMenu();
     }
@@ -69,72 +63,69 @@ public class ProductView {
         String tarifString = scan.nextLine().trim();
         try {
             float tarif = Float.parseFloat(tarifString);
-            
+
             System.out.print("Visuel : ");
             String visuel = scan.nextLine().trim();
             System.out.print("ID category : ");
             String id_categoryString = scan.nextLine().trim();
-            try {
-                var id_category = Integer.parseInt(id_categoryString);
-        
-                // prodController.addProduct(nom, description, tarif, visuel, id_category);
-                openProductMenu();    
-            }
-            catch (NumberFormatException nfe) {	    	
-                System.out.println("NumberFormatException: " + nfe.getMessage());
-                insertProduct();
-            }
-        }
-        catch (NumberFormatException nfe) {	    	
+            var id_category = Integer.parseInt(id_categoryString);
+
+            // prodController.addProduct(nom, description, tarif, visuel, id_category);
+            openProductMenu();
+        } catch (NumberFormatException nfe) {
             System.out.println("NumberFormatException: " + nfe.getMessage());
             insertProduct();
         }
         scan.close();
     }
 
-    // public static void updateProduct() {
-    //     System.out.println();
+    public static void updateProduct() {
+        System.out.println();
 
-    //     Scanner scan = new Scanner(System.in);
-    //     System.out.print("Id : ");
-    //     String idString = scan.nextLine().trim();
-    //     try {
-    //         var id = Integer.parseInt(idString);
-            
-    //         System.out.print("New title : ");
-    //         String newTitle = scan.nextLine().trim();
-    //         System.out.print("New visuel : ");
-    //         String newVisuel = scan.nextLine().trim();
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Id : ");
+        String idString = scan.nextLine().trim();
+        try {
+            int id = Integer.parseInt(idString);
+            System.out.print("Nom : ");
+            String nom = scan.nextLine().trim();
+            System.out.print("Description : ");
+            String description = scan.nextLine().trim();
+            System.out.print("Tarif : ");
+            float tarif = Float.parseFloat(scan.nextLine().trim());
+            System.out.print("Visuel : ");
+            String visuel = scan.nextLine().trim();
+            System.out.print("ID category : ");
+            int id_category = Integer.parseInt(scan.nextLine().trim());
 
-    //         prodController.editProduct(id, newTitle, newVisuel);
-    //         openProductMenu();
-    //     }
-    //     catch (NumberFormatException nfe) {	    	
-    //         System.out.println("NumberFormatException: " + nfe.getMessage());
-    //         updateProduct();
-    //     }
+            // prodController.updateProduct(id, nom, description, tarif, visuel,
+            // id_category);
+            openProductMenu();
+        } catch (NumberFormatException nfe) {
+            System.out.println("NumberFormatException: " + nfe.getMessage());
+            updateProduct();
+        }
 
-    //     scan.close();
-    // }
+        scan.close();
+    }
 
-    // public static void deleteProduct() {
-    //     System.out.println();
+    public static void deleteProduct() {
+        System.out.println();
 
-    //     Scanner scan = new Scanner(System.in);
-    //     System.out.print("Id : ");
-    //     String idString = scan.nextLine();
-    //     try {
-    //         var id = Integer.parseInt(idString.trim());
-            
-    //         prodController.removeProduct(id);
-    //         openProductMenu();
-    //     }
-    //     catch (NumberFormatException nfe) {	    	
-    //         System.out.println("NumberFormatException: " + nfe.getMessage());
-    //         updateProduct();
-    //     }
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Id : ");
+        String idString = scan.nextLine().trim();
+        try {
+            int id = Integer.parseInt(idString);
 
-    //     scan.close();
-    // }
+            prodController.removeProduct(id);
+            openProductMenu();
+        } catch (NumberFormatException nfe) {
+            System.out.println("NumberFormatException: " + nfe.getMessage());
+            deleteProduct();
+        }
+
+        scan.close();
+    }
 
 }

a92b5e501bd42b8961ab1c161737fed2a06bd680
Clement Ricatte <ricatte3u@etu.univ-lorraine.fr>
2020-09-11 09:37:38 +0200

    change the way the timezone is specified

--- src/Sql.java
+++ src/Sql.java
@@ -9,14 +9,15 @@ public class Sql {
 
     public static Connection startConnection() {
         String url =
-        "jdbc:mysql://localhost/iut_cpoa?serverTimezone=UTC";
+        "jdbc:mysql://localhost/iut_cpoa";
+        url += "?serverTimezone=UTC";
         String login = "root";
         String pwd = "root";
         Connection myConnection = null;
         try {
-        myConnection = DriverManager.getConnection(url, login, pwd);
+            myConnection = DriverManager.getConnection(url, login, pwd);
         } catch (SQLException sqle) {
-        System.out.println("Erreur connexion " + sqle.getMessage());
+            System.out.println("Erreur connexion " + sqle.getMessage());
         }
         return myConnection;
     }
@@ -24,5 +25,5 @@ public class Sql {
     public static void main(String[] args) {
         var connection = startConnection();
     }
-    
+
 }

3aee70927fe62bf1a0a9e0a648788e666fbe4da3
OxyTom <oxytom8@gmail.com>
2020-09-11 16:47:52 +0200

    [TD1] logs for db is now read in a xaml file

--- src/controllers/BaseSql.java
+++ src/controllers/BaseSql.java
@@ -1,18 +1,60 @@
 package controllers;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.sql.*;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+
 public abstract class BaseSql {
-    public static Connection startConnection() {
-        String url = "jdbc:mysql://localhost/iut_cpoa?serverTimezone=UTC";
-        String login = "root";
-        String pwd = "root";
-        Connection myConnection = null;
+
+    private static String server;
+    private static String port;
+    private static String database;
+    private static String username;
+    private static String password;
+
+    public static void initConnection() {
         try {
-            myConnection = DriverManager.getConnection(url, login, pwd);
+            File file = new File("creditentials.xml");
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("sql");
+
+            for (int itr = 0; itr < nodeList.getLength(); itr++) {
+                Node node = nodeList.item(itr);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) node;
+                    server = eElement.getElementsByTagName("server").item(0).getTextContent();
+                    port = eElement.getElementsByTagName("port").item(0).getTextContent();
+                    database = eElement.getElementsByTagName("database").item(0).getTextContent();
+                    username = eElement.getElementsByTagName("username").item(0).getTextContent();
+                    password = eElement.getElementsByTagName("password").item(0).getTextContent();
+                }
+            }
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            System.err.println("Erreur lecture " + e.getMessage());
+        }
+    }
+
+    public static Connection startConnection() {
+        String url = String.format("jdbc:mysql://%s/%s?serverTimezone=UTC", server, database);
+        try {
+            return DriverManager.getConnection(url, username, password);
         } catch (SQLException sqle) {
             System.out.println("Erreur connexion " + sqle.getMessage());
         }
-        return myConnection;
+        return null;
     }
 }

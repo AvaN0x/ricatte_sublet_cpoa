139baee931c6ee5e1b49ba2eee39e63f16583b0e
AvaN0x <clement.ricatte57@gmail.com>
2020-09-11 15:46:16 +0200

    clean and add a new submenu in the main menu

--- src/views/MainView.java
+++ src/views/MainView.java
@@ -8,33 +8,32 @@ public class MainView {
     }
 
     public static void openMainMenu() {
-        System.out.println("\n" + 
-        "-- Main menu --" + "\n" + 
-        "0/ Exit" + "\n" +
-        "1/ Category" + "\n"
-    );
-    System.out.print("Choice : ");
+        System.out.println(
+                "\n" + "-- Main menu --" + "\n" + "0/ Exit" + "\n" + "1/ Category" + "\n" + "1/ Product" + "\n");
+        System.out.print("Choice : ");
 
-    Scanner scan = new Scanner(System.in);
-    String prompted = scan.nextLine();
-    try {
-        int submenu = Integer.parseInt(prompted.trim());
-        switch (submenu) {
-            case 0 : 
-                break;
-            case 1 : 
-                CategoryView.openCategoryMenu();
-                break;
-            default :
-                openMainMenu();
-                break;
+        Scanner scan = new Scanner(System.in);
+        String prompted = scan.nextLine();
+        try {
+            int submenu = Integer.parseInt(prompted.trim());
+            switch (submenu) {
+                case 0:
+                    break;
+                case 1:
+                    CategoryView.openCategoryMenu();
+                    break;
+                case 2:
+                    ProductView.openProductMenu();
+                    break;
+                default:
+                    openMainMenu();
+                    break;
+            }
+        } catch (NumberFormatException nfe) {
+            System.out.println("NumberFormatException: " + nfe.getMessage());
+            openMainMenu();
         }
-    }
-    catch (NumberFormatException nfe) {	    	
-        System.out.println("NumberFormatException: " + nfe.getMessage());
-        openMainMenu();
-    }
-    scan.close();
+        scan.close();
 
     }
 }

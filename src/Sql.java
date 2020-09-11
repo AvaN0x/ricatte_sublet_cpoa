fa57f1ab3c494cb2d4663bb8d8655849ebb33112
Clement Ricatte <ricatte3u@etu.univ-lorraine.fr>
2020-09-11 10:24:02 +0200

    You can now create, update end delete categories

--- src/Sql.java
+++ src/Sql.java
@@ -4,6 +4,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import models.Category;
+
 /**
  * Sql class
  */
@@ -26,16 +28,47 @@ public class Sql {
 
     public static void main(String[] args) {
         // var connection = startConnection();
+        remCategory(7);
+        var c = new Category(6, "Pantalon", "pantalon.png");
+        addCategory(c);
+        remCategory(c.id);
+        addCategory(new Category(7, "Testing", "Things"));
+        updateCategory(7, "Testing", "Nothing");
     }
     
-    public void addCategory(Category c) {
+    public static void addCategory(Category c) {
         try {
             Connection myConnection = startConnection();
             Statement request = myConnection.createStatement();
-            ResultSet res = request.executeQuery(String.format("INSERT INTO `categorie`(`id_categorie`, `titre`, `visuel`) VALUES (%s, %s, %s)"), c.id_categorie, c.titre, c.visuel); 
+            var res = request.executeUpdate(String.format("INSERT INTO `categorie`(`id_categorie`, `titre`, `visuel`) VALUES (%s, \"%s\", \"%s\")", c.id, c.title, c.visuel)); 
         } catch (SQLException sqle) {
-            System.out.println("Pb select" + sqle.getMessage());
+            System.out.println("Error executeUpdate " + sqle.getMessage());
         }
     }
+
+    public static void updateCategory(int id, String title, String visuel) {
+        try {
+            Connection myConnection = startConnection();
+            Statement request = myConnection.createStatement();
+            var res = request.executeUpdate(String.format("UPDATE `categorie` SET `titre`= \"%s\",`visuel`= \"%s\" WHERE id_categorie = %s", title, visuel, id)); 
+        } catch (SQLException sqle) {
+            System.out.println("Error executeUpdate " + sqle.getMessage());
+        }
+    }
+
+    public static void remCategory(Category c) {
+        remCategory(c.id);
+    }
+    public static void remCategory(int id) {
+        try {
+            Connection myConnection = startConnection();
+            Statement request = myConnection.createStatement();
+            var res = request.executeUpdate(String.format("DELETE FROM `categorie` WHERE id_categorie = %s", id)); 
+        } catch (SQLException sqle) {
+            System.out.println("Error executeUpdate " + sqle.getMessage());
+        }
+    }
+
+
 
 }

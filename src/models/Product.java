98d9737fbe47d09854bebaf8616c9c2787219fa8
AvaN0x <clement.ricatte57@gmail.com>
2020-09-18 14:27:06 +0200

    Create Product POJO

---
+++ src/models/Product.java
@@ -0,0 +1,84 @@
+package models;
+
+import java.util.Objects;
+
+public class Product {
+    private int _id;
+    private String _nom;
+    private String _description;
+    private float _tarif;
+    private String _visuel;
+
+    public Product(int _id, String _nom, String _description, float _tarif, String _visuel) {
+        this._id = _id;
+        this._nom = _nom;
+        this._description = _description;
+        this._tarif = _tarif;
+        this._visuel = _visuel;
+    }
+
+    public int get_id() {
+        return this._id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String get_nom() {
+        return this._nom;
+    }
+
+    public void set_nom(String _nom) {
+        this._nom = _nom;
+    }
+
+    public String get_description() {
+        return this._description;
+    }
+
+    public void set_description(String _description) {
+        this._description = _description;
+    }
+
+    public float get_tarif() {
+        return this._tarif;
+    }
+
+    public void set_tarif(float _tarif) {
+        this._tarif = _tarif;
+    }
+
+    public String get_visuel() {
+        return this._visuel;
+    }
+
+    public void set_visuel(String _visuel) {
+        this._visuel = _visuel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Product)) {
+            return false;
+        }
+        Product product = (Product) o;
+        return _id == product._id && Objects.equals(_nom, product._nom)
+                && Objects.equals(_description, product._description) && _tarif == product._tarif
+                && Objects.equals(_visuel, product._visuel);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_id, _nom, _description, _tarif, _visuel);
+    }
+
+    @Override
+    public String toString() {
+        return "{" + " _id='" + get_id() + "'" + ", _nom='" + get_nom() + "'" + ", _description='" + get_description()
+                + "'" + ", _tarif='" + get_tarif() + "'" + ", _visuel='" + get_visuel() + "'" + "}";
+    }
+
+}

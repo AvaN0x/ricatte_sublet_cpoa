e267e46afd11946c1c377a6cddaf02eda1dd017d
OxyTom <oxytom8@gmail.com>
2020-09-18 13:16:58 +0200

    [TD2] added xaml reader for sql

---
+++ src/BaseSQL.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+
+public class BaseSQL {
+    private static String _server;
+    private static String _port;
+    private static String _database;
+    private static String _username;
+    private static String _password;
+
+    public static void initConnection() {
+        try {
+            File file = new File("creditentials.xml");
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("sql");
+
+            for (int itr = 0; itr < nodeList.getLength(); itr++) {
+                Node node = nodeList.item(itr);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) node;
+                    _server = eElement.getElementsByTagName("server").item(0).getTextContent();
+                    _port = eElement.getElementsByTagName("port").item(0).getTextContent();
+                    _database = eElement.getElementsByTagName("database").item(0).getTextContent();
+                    _username = eElement.getElementsByTagName("username").item(0).getTextContent();
+                    _password = eElement.getElementsByTagName("password").item(0).getTextContent();
+                }
+            }
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            System.err.println("Erreur lecture " + e.getMessage());
+        }
+    }
+}

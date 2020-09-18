6bce13a2839debb4c54cd3963c70b6bdd6a9e607
OxyTom <oxytom8@gmail.com>
2020-09-18 13:29:15 +0200

    [TD2] better error managment

--- src/BaseSQL.java
+++ src/BaseSQL.java
@@ -16,30 +16,26 @@ public class BaseSQL {
     private static String _username;
     private static String _password;
 
-    public static void initConnection() {
-        try {
-            File file = new File("creditentials.xml");
+    public static void initConnection() throws ParserConfigurationException, SAXException, IOException {
+        File file = new File("creditentials.xml");
 
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document doc = db.parse(file);
-            doc.getDocumentElement().normalize();
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(file);
+        doc.getDocumentElement().normalize();
 
-            NodeList nodeList = doc.getElementsByTagName("sql");
+        NodeList nodeList = doc.getElementsByTagName("sql");
 
-            for (int itr = 0; itr < nodeList.getLength(); itr++) {
-                Node node = nodeList.item(itr);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) node;
-                    _server = eElement.getElementsByTagName("server").item(0).getTextContent();
-                    _port = eElement.getElementsByTagName("port").item(0).getTextContent();
-                    _database = eElement.getElementsByTagName("database").item(0).getTextContent();
-                    _username = eElement.getElementsByTagName("username").item(0).getTextContent();
-                    _password = eElement.getElementsByTagName("password").item(0).getTextContent();
-                }
+        for (int itr = 0; itr < nodeList.getLength(); itr++) {
+            Node node = nodeList.item(itr);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) node;
+                _server = eElement.getElementsByTagName("server").item(0).getTextContent();
+                _port = eElement.getElementsByTagName("port").item(0).getTextContent();
+                _database = eElement.getElementsByTagName("database").item(0).getTextContent();
+                _username = eElement.getElementsByTagName("username").item(0).getTextContent();
+                _password = eElement.getElementsByTagName("password").item(0).getTextContent();
             }
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            System.err.println("Erreur lecture " + e.getMessage());
         }
     }
 }

f4cbb56e3f944cfeca4e6616cc1b8bc333982ca5
AvaN0x <clement.ricatte57@gmail.com>
2020-09-18 16:53:36 +0200

    add settingsmenu to specify in which mode we are in

--- src/views/MainView.java
+++ src/views/MainView.java
@@ -3,20 +3,25 @@ package views;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import dao.Persistance;
+
 public class MainView {
-    private static boolean inMenu;
+    private static Persistance persistance;
+    private static Scanner scan;
 
     public static void main(String[] args) {
+        persistance = Persistance.MYSQL;
         openMainMenu();
     }
 
     public static void openMainMenu() {
-        inMenu = true;
-        Scanner scan = new Scanner(System.in);
+        var inMenu = true;
+        scan = new Scanner(System.in);
         do {
 
-            System.out.println("\n-- Main menu --\n0/ Quitter\n1/ Category\n2/ Product\n3/ Client\n");
-            System.out.print("Choice : ");
+            System.out.println("\n-- Main menu -- " + persistance
+                    + " \n0/ Quitter\n1/ Settings\n2/ Category\n3/ Product\n4/ Client\n");
+            System.out.print("Choix : ");
 
             try {
                 var submenu = scan.nextInt();
@@ -24,6 +29,10 @@ public class MainView {
                 switch (submenu) {
                     case 0:
                         inMenu = false;
+                        break;
+                    case 1:
+                        openSettingsMenu();
+                        break;
                     default:
                         break;
                 }
@@ -33,6 +42,38 @@ public class MainView {
             }
         } while (inMenu);
         scan.close();
-
     }
+
+    public static void openSettingsMenu() {
+        var inMenu = true;
+        do {
+
+            System.out.println("\n-- Settings menu --\n0/ Quitter\n1/ Liste Mémoire\n2/ MySQL\n");
+            System.out.print("Choix : ");
+
+            try {
+                var submenu = scan.nextInt();
+                scan.nextLine();
+                switch (submenu) {
+                    case 0:
+                        inMenu = false;
+                        break;
+                    case 1:
+                        persistance = Persistance.LISTE_MEMOIRE;
+                        inMenu = false;
+                        break;
+                    case 2:
+                        persistance = Persistance.MYSQL;
+                        inMenu = false;
+                        break;
+                    default:
+                        break;
+                }
+            } catch (NumberFormatException | InputMismatchException e) {
+                System.out.println("Exception: " + e.getMessage());
+                scan.nextLine();
+            }
+        } while (inMenu);
+    }
+
 }

1f5e2f8004b788102bd9c1c2069e207db1fb00bf
AvaN0x <clement.ricatte57@gmail.com>
2020-09-25 08:24:04 +0200

    ClientView edit change private var name, add creation of Client

--- src/views/ClientView.java
+++ src/views/ClientView.java
@@ -3,26 +3,28 @@ package views;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import dao.DAOFactory;
 import dao.Persistance;
+import models.Client;
 
 public class ClientView {
-    private static Persistance persistance;
-    private static Scanner scan;
+    private static Persistance _persistance;
+    private static Scanner _scan;
 
-    public static void openClientMenu(Persistance _persistance, Scanner _scan) {
-        persistance = _persistance;
-        scan = _scan;
+    public static void openClientMenu(Persistance persistance, Scanner scan) {
+        _persistance = persistance;
+        _scan = scan;
         var inMenu = true;
         // TODO get every clients in a var
         do {
 
-            System.out.println("\n-- Client menu -- " + persistance + " \n0/ Quitter\n1/ Ajouter");
+            System.out.println("\n-- Client menu -- " + _persistance + " \n0/ Quitter\n1/ Ajoute\n2/Liste");
             // TODO display every clients
             System.out.print("Choix : ");
 
             try {
                 var submenu = scan.nextInt();
-                scan.nextLine();
+                _scan.nextLine();
                 switch (submenu) {
                     case 0:
                         inMenu = false;
@@ -30,43 +32,53 @@ public class ClientView {
                     case 1:
                         createClientMenu();
                         break;
+                    case 2:
+                        displayClientMenu();
+                        break;
                     default:
                         break;
                 }
             } catch (NumberFormatException | InputMismatchException e) {
                 System.out.println("Exception: " + e.getMessage());
-                scan.nextLine();
+                _scan.nextLine();
             }
         } while (inMenu);
     }
 
     public static void createClientMenu() {
-        System.out.println("\n-- Ajouter un Client -- " + persistance);
+        System.out.println("\n-- Ajouter un Client -- " + _persistance);
 
         try {
             System.out.print("Nom : ");
-            String nom = scan.nextLine().trim();
+            String nom = _scan.nextLine().trim();
             System.out.print("Prenom : ");
-            String prenom = scan.nextLine().trim();
+            String prenom = _scan.nextLine().trim();
             System.out.print("Identifiant : ");
-            String identifiant = scan.nextLine().trim();
+            String identifiant = _scan.nextLine().trim();
             System.out.print("Mot de passe : ");
-            String motDePasse = scan.nextLine().trim();
+            String motDePasse = _scan.nextLine().trim();
             System.out.print("Numéro adresse : ");
-            int adrNumero = scan.nextInt();
+            int adrNumero = _scan.nextInt();
             System.out.print("Voie adresse : ");
-            String adrVoie = scan.nextLine().trim();
+            String adrVoie = _scan.nextLine().trim();
             System.out.print("Code postal : ");
-            int adrCodePostal = scan.nextInt();
+            int adrCodePostal = _scan.nextInt();
             System.out.print("Ville : ");
-            String adrVille = scan.nextLine().trim();
+            String adrVille = _scan.nextLine().trim();
             System.out.print("Pays : ");
-            String adrPays = scan.nextLine().trim();
+            String adrPays = _scan.nextLine().trim();
 
-            // TODO create the client
+            var daos = DAOFactory.getDAOFactory(_persistance);
+            daos.getClientDAO().create(new Client(-1, nom, prenom, identifiant, motDePasse, adrNumero, adrVoie,
+                    adrCodePostal, adrVille, adrPays));
+
         } catch (NumberFormatException | InputMismatchException e) {
             System.out.println("Exception: " + e.getMessage());
-            scan.nextLine();
+            _scan.nextLine();
         }
+    }
+
+    public static void displayClientMenu() {
+
     }
 }

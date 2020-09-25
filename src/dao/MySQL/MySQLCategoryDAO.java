e69588f8bc143bcad8a883e5d99f2b71f001101e
OxyTom <oxytom8@gmail.com>
2020-09-25 08:26:13 +0200

    [TD2] added getAll()

--- src/dao/MySQL/MySQLCategoryDAO.java
+++ src/dao/MySQL/MySQLCategoryDAO.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import dao.CategoryDAO;
 import models.Category;
@@ -17,6 +18,7 @@ public class MySQLCategoryDAO extends BaseMySQL implements CategoryDAO {
 
     private static MySQLCategoryDAO instance;
 
+    @Override
     public Category getById(int id) throws SQLException {
         Connection con = startConnection();
         PreparedStatement query = con.prepareStatement("SELECT * FROM categorie WHERE id_categorie=? LIMIT 1");
@@ -30,6 +32,7 @@ public class MySQLCategoryDAO extends BaseMySQL implements CategoryDAO {
         return categ;
     }
 
+    @Override
     public boolean create(Category categ) throws SQLException {
         Connection con = startConnection();
         PreparedStatement update = con.prepareStatement("INSERT INTO categorie(titre, visuel) VALUES (?, ?)");
@@ -40,6 +43,7 @@ public class MySQLCategoryDAO extends BaseMySQL implements CategoryDAO {
         return result >= 1;
     }
 
+    @Override
     public boolean update(Category categ) throws SQLException {
         Connection con = startConnection();
         PreparedStatement update = con.prepareStatement("UPDATE categorie SET titre=?, visuel=? WHERE id_categorie=?");
@@ -51,6 +55,7 @@ public class MySQLCategoryDAO extends BaseMySQL implements CategoryDAO {
         return result >= 1;
     }
 
+    @Override
     public boolean delete(Category categ) throws SQLException {
         Connection con = startConnection();
         PreparedStatement update = con.prepareStatement("DELETE FROM categorie WHERE id_categorie=?");
@@ -60,6 +65,7 @@ public class MySQLCategoryDAO extends BaseMySQL implements CategoryDAO {
         return result >= 1;
     }
 
+    @Override
     public Category getByTitle(String title) throws SQLException {
         Connection con = startConnection();
         PreparedStatement query = con.prepareStatement("SELECT id_categorie FROM categorie WHERE titre=? LIMIT 1");
@@ -71,6 +77,19 @@ public class MySQLCategoryDAO extends BaseMySQL implements CategoryDAO {
             categId = categRes.getInt(1);
         con.close();
         return getById(categId);
+    }
+
+    @Override
+    public ArrayList<Category> getAll() throws SQLException {
+        Connection con = startConnection();
+        PreparedStatement query = con.prepareStatement("SELECT * FROM categorie");
+        ResultSet categRes = query.executeQuery();
+
+        ArrayList<Category> categs = new ArrayList<Category>();
+        while (categRes.next())
+            categs.add(new Category(categRes.getInt(1), categRes.getString("titre"), categRes.getString("visuel")));
+        con.close();
+        return categs;
     }
 
     public static MySQLCategoryDAO getInstance() throws IOException {

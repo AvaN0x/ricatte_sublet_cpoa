eb0dfc1147507903c0d841957c525f785104d5ae
AvaN0x <clement.ricatte57@gmail.com>
2020-10-02 09:39:03 +0200

    rename a folder 1

---
+++ src/tests/dao/ramtest/RAMClientDAOTest.java
@@ -0,0 +1,34 @@
+package tests.dao.RAM;
+
+import models.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import dao.RAM.RAMClientDAO;
+
+public class RAMClientDAOTest {
+
+    @Test
+    public void testSelectExist() {
+        Client cli = new Client(0);
+        RAMClientDAO.getInstance().create(cli);
+        assertNotNull(RAMClientDAO.getInstance().getById(cli.getId()));
+    }
+
+    @Test
+    public void testUpdate() {
+        Client cli = new Client(0);
+        RAMClientDAO.getInstance().create(cli);
+        cli.setNom("nom");
+        RAMClientDAO.getInstance().update(cli);
+        assertEquals(RAMClientDAO.getInstance().getById(cli.getId()).getNom(), "nom");
+    }
+
+    @Test
+    public void testDelete() {
+        Client cli = new Client(0);
+        RAMClientDAO.getInstance().create(cli);
+        assertTrue(RAMClientDAO.getInstance().delete(cli));
+    }
+
+}

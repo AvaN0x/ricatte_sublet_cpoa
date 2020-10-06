f28c21d5cce5ce2de23aa335360c3d13ae315332
OxyTom <oxytom8@gmail.com>
2020-10-06 08:35:49 +0200

    [TD3] Command DAO test (mysql)

--- src/tests/dao/mysql/MySQLCommandDAOTest.java
+++ src/tests/dao/mysql/MySQLCommandDAOTest.java
@@ -2,6 +2,8 @@ package tests.dao.mysql;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,7 +18,7 @@ public class MySQLCommandDAOTest {
     @BeforeEach
     public void setUp() throws Exception {
         this.daos = dao.DAOFactory.getDAOFactory(dao.Persistance.MYSQL);
-        daos.getCommandDAO().create(new Command(java.time.LocalDate.now(), new models.Client(0)));
+        daos.getCommandDAO().create(new Command(LocalDate.now(), new models.Client(0)));
         var items = daos.getCommandDAO().getAll();
         this.com_id = items.get(items.size() - 1).getId();
     }
@@ -24,5 +26,50 @@ public class MySQLCommandDAOTest {
     @AfterEach
     public void tearUp() throws Exception {
         daos.getCommandDAO().delete(new Command(com_id));
+    }
+
+    @Test
+    @Order(3)
+    public void testGetById() throws Exception {
+        assertNotNull(daos.getCommandDAO().getById(com_id));
+    }
+
+    @Test
+    @Order(2)
+    public void testCreate() throws Exception {
+        var com = new Command(LocalDate.now(), new models.Client(0));
+        daos.getCommandDAO().create(com);
+        assertNotNull(daos.getCommandDAO().getById(com_id));
+        daos.getCommandDAO().delete(com);
+    }
+
+    @Test
+    @Order(4)
+    public void testUpdate() throws Exception {
+        var date = LocalDate.now();
+        daos.getCommandDAO()
+                .update(new Command(date, new models.Client(0)););
+        assertEquals(date, daos.getCommandDAO().getById(com_id).getDateCommand());
+    }
+
+    @Test
+    @Order(6)
+    public void testDelete() throws Exception {
+        var com = new Command(LocalDate.now(), new models.Client(0));
+        daos.getCommandDAO().create(com);
+        daos.getCommandDAO().delete(com);
+        assertNull(daos.getCommandDAO().getById(com.getId()));
+    }
+
+    @Test
+    @Order(5)
+    public void testGetAll() throws Exception {
+        assertFalse(daos.getCommandDAO().getAll().isEmpty());
+    }
+
+    @Test
+    @Order(1)
+    public void testGetInstance() throws Exception {
+        assertEquals(dao.mysql.MySQLCommandDAO.class, daos.getCommandDAO().getClass());
     }
 }

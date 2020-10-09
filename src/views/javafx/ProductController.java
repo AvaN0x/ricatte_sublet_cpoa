fbde5215bdbb9eedf473a6d97fb179063f7e12b4
OxyTom <oxytom8@gmail.com>
2020-10-09 11:56:06 +0200

    [TD4] Product is now correctly shown

--- src/views/javafx/ProductController.java
+++ src/views/javafx/ProductController.java
@@ -31,7 +31,7 @@ public class ProductController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
-            daos = DAOFactory.getDAOFactory(Persistance.MYSQL);
+            daos = DAOFactory.getDAOFactory(Persistance.RAM);
             var list = daos.getCategoryDAO().getAll();
             this.cb_categorie.setItems(FXCollections.observableArrayList(list));
         } catch (Exception e) {
@@ -44,14 +44,14 @@ public class ProductController implements Initializable {
             var prod = new Product(tf_nom.getText(), ta_description.getText(), Float.parseFloat(tf_tarif.getText()),
                     "null", cb_categorie.getValue());
 
-            daos.getProductDAO().create(prod);
-
             tf_nom.setText("");
             ta_description.setText("");
             tf_tarif.setText("");
             cb_categorie.setValue(null);
 
             lbl_result.setText(prod.toString());
+
+            daos.getProductDAO().create(prod);
 
         } catch (Exception e) {
             lbl_result.setText(e.getMessage());

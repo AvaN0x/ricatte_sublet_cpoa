0fecadf795a875053b862b1084ebe203b4e33455
AvaN0x <clement.ricatte57@gmail.com>
2020-10-09 11:41:32 +0200

    add every categories in the choicebox

--- src/views/javafx/ProductController.java
+++ src/views/javafx/ProductController.java
@@ -1,11 +1,35 @@
 package views.javafx;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
-public class ProductController {
+import models.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import dao.*;
+
+public class ProductController implements Initializable {
     @FXML
     private Label lbl_result;
+
+    @FXML
+    private ChoiceBox<Category> cb_categorie;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            DAOFactory daos = DAOFactory.getDAOFactory(Persistance.MYSQL);
+            var list = daos.getCategoryDAO().getAll();
+            this.cb_categorie.setItems(FXCollections.observableArrayList(list));
+        } catch (Exception e) {
+            lbl_result.setText(e.getMessage());
+        }
+    }
 
     public void createClick() {
         try {

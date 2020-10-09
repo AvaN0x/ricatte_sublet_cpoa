ee1da5bd6c9c0be69944b7e48e3560a6a475f362
OxyTom <oxytom8@gmail.com>
2020-10-09 11:47:39 +0200

    [TD4] Product created on click

--- src/views/javafx/ProductController.java
+++ src/views/javafx/ProductController.java
@@ -5,25 +5,33 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-
-import models.*;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import dao.*;
+import models.Product;
 
 public class ProductController implements Initializable {
     @FXML
-    private Label lbl_result;
-
+    private TextField tf_nom;
     @FXML
-    private ChoiceBox<Category> cb_categorie;
+    private TextArea ta_description;
+    @FXML
+    private TextField tf_tarif;
+    @FXML
+    private Label lbl_result;
+    @FXML
+    private ChoiceBox<models.Category> cb_categorie;
+
+    private DAOFactory daos;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
-            DAOFactory daos = DAOFactory.getDAOFactory(Persistance.MYSQL);
+            daos = DAOFactory.getDAOFactory(Persistance.MYSQL);
             var list = daos.getCategoryDAO().getAll();
             this.cb_categorie.setItems(FXCollections.observableArrayList(list));
         } catch (Exception e) {
@@ -33,8 +41,11 @@ public class ProductController implements Initializable {
 
     public void createClick() {
         try {
-            var categ = new models.Category("", "");
-        } catch (IllegalArgumentException e) {
+            var prod = new Product(tf_nom.getText(), ta_description.getText(), Float.parseFloat(tf_tarif.getText()),
+                    "null", cb_categorie.getValue());
+            daos.getProductDAO().create(prod);
+
+        } catch (Exception e) {
             lbl_result.setText(e.getMessage());
         }
     }

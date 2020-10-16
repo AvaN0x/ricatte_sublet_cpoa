7ae4f4977906be4e665ffe649c4e9acf55c430f1
OxyTom <oxytom8@gmail.com>
2020-10-16 09:49:19 +0200

    [Projet] created main controller

---
+++ src/controllers/MainController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController extends BaseController {
+
+    @Override
+    public void initialize(URL location, ResourceBundle ressources) {
+        try {
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+}

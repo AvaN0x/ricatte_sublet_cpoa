1d8ba92d7773c383239bb94c6e161a8de73fe71e
OxyTom <oxytom8@gmail.com>
2020-10-16 09:47:29 +0200

    [Projet] created command controller

---
+++ src/controllers/CommandController.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DateCell;
+
+import models.Command;
+
+public class CommandController extends BaseController {
+    @FXML
+    private DateCell dcDate;
+    @FXML
+    private ChoiceBox<models.Client> cbClient;
+    @FXML
+    private ChoiceBox<models.Product> cbProduits;
+
+    @Override
+    public void initialize(URL location, ResourceBundle ressources) {
+        try {
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    public void updateClientBox() {
+
+    }
+
+    public void createClick() {
+        try {
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+}
